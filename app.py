3fdead6154016e2c4e3def2def28b3f9552c28ad
Ashwini Chavan <48444403+ashwinichavan5443@users.noreply.github.com>
2021-08-26 13:09:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ model = pickle.load(open('salary_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('templates/index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -20,7 +20,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('templates/index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
 if __name__ == "__main__":
