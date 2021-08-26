cd31bc3701bd6b8880e9a3b04379a8f60e36a724
Ashwini Chavan <48444403+ashwinichavan5443@users.noreply.github.com>
2021-08-26 12:28:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 import numpy as np
+from flask import Flask, request, jsonify, render_template
 import pickle
-from flask import Flask, request,jsonify,render_template
 
-# create flask app using Flask
-app=Flask(__name__)
-
-#load model
-model=pickle.load(open('salary_model.pkl','rb'))
+app = Flask(__name__)
+model = pickle.load(open('salary_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
 
-	int_features=[int(x) for x in request.form.values()]
-	final_features = [np.array(int_features)]
-	prediction=model.predict(final_features)
-	output=round(prediction[0],2)
-	return render_template('index.html',prediction_text='Employee Salary should be $ {}'.format(output))
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
